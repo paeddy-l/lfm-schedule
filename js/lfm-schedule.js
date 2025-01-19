f8e98cb8de80c9296816737f24f191b9fafb69d3
Päddy <74115164+paeddy-l@users.noreply.github.com>
2025-01-19 17:59:03 +0100

    Update lfm-schedule.js

--- js/lfm-schedule.js
+++ js/lfm-schedule.js
@@ -1,32 +1,32 @@
 $(document).ready(function() {
-  // Name der laut.fm Station
-  const station_name = "";
+  // Konfiguration des Sendeplans
+  const config = {
+    // Name der laut.fm Station
+    station_name: "",
 
-  // Option zum Anzeigen der Bilder (true / false)
-  const images = true;
+    // Option zum Anzeigen der Bilder (true / false)
+    displayImages: true,
 
-  // URL inkl. Unterordner, wo die Bilder zu suchen sind
-  const images_url = "";
+    // URL inkl. Unterordner, wo die Bilder zu suchen sind
+    imagesUrl: "",
 
-  // Wenn true und images auch true ist, wird der
-  // benötigte Dateinamen der Bilder angezeigt (true / false)
-  const show_img_name = false;
+    // Wenn true und displayImages auch true ist, wird der
+    // benötigte Dateinamen der Bilder angezeigt (true / false)
+    showImageName: false,
 
-  // Das Standardbild, wenn das eigentliche Bild nicht gefunden wird.
-  const img_default = ""; // Die gesamte URL ist "images_url + img_default"
+    // Das Standardbild, wenn das eigentliche Bild nicht gefunden wird.
+    // Die gesamte URL ist "imagesUrl + defaultImage"
+    defaultImage: "",
 
-  // Die angezeigten Wochentage
-  const daysOfWeek = ["Montag", "Dienstag", "Mittwoch", "Donnerstag", "Freitag", "Samstag", "Sonntag"];
+    // Die angezeigten Wochentage
+    daysOfWeek: ["Montag", "Dienstag", "Mittwoch", "Donnerstag", "Freitag", "Samstag", "Sonntag"]
+  };
 
-  //////////////////////////
-  // ENDE DER AENDERUNGEN //
-  /////////////////////////
-
-  // DOM-Referenz
-  const scheduleContainer = document.getElementById('lfm_schedule_selfhost');
+  // Definiere das Array für die Wochentage global
+  const days = ['mon', 'tue', 'wed', 'thu', 'fri', 'sat', 'sun'];
 
   // Grundstruktur in HTML erstellen
-  scheduleContainer.innerHTML = `
+  $('#lfm_schedule_selfhost').html(`
     <div id="lfm_schedule" class="lfm_schedule">
       <div class="tab"></div>
       <table class="lfm_api_schedule_table_head">
@@ -39,123 +39,113 @@ $(document).ready(function() {
       </table>
       <div class="tabcontent-container"></div>
     </div>
-  `;
+  `);
 
-  const tabContainer = document.querySelector('.tab');
-  const tabContentContainer = document.querySelector('.tabcontent-container');
+  // Tabs und deren Inhalte erstellen
+  const tabContainer = $('.tab');
+  const tabContentContainer = $('.tabcontent-container');
 
-  if (!images) {
-    $('.lfm_api_schedule_head_img').hide();
-  }
-
-  // Dynamisch Tabs erstellen
-  daysOfWeek.forEach((day, index) => {
-    // Tab Button
-    const tabButton = document.createElement('button');
-    tabButton.className = 'tablinks';
-    tabButton.textContent = day;
-    tabButton.setAttribute('data-day', index);
-    tabButton.addEventListener('click', function() {
-      openDAY(index);
-    });
-
-    tabContainer.appendChild(tabButton);
-
-    // Tab Content
-    const tabContent = document.createElement('div');
-    tabContent.className = 'tabcontent';
-    tabContent.innerHTML = `<div class="api_lfm_schedule">Loading...</div>`;
-
-    tabContentContainer.appendChild(tabContent);
+  config.daysOfWeek.forEach((day, index) => {
+    tabContainer.append(`<button class="tablinks" data-day="${index}">${day}</button>`);
+    tabContentContainer.append('<div class="tabcontent"><div class="api_lfm_schedule">Loading...</div></div>');
   });
 
-  // Alle Tab-Buttons und Tab-Inhalte sammeln
-  var tablinks = Array.from(document.getElementsByClassName("tablinks"));
-  var tabcontent = Array.from(document.getElementsByClassName("tabcontent"));
+  // Anzeige der Tabs bei Klick
+  const tablinks = $(".tablinks");
+  const tabcontent = $(".tabcontent");
+
+  tablinks.on('click', function() {
+    openDAY($(this).data('day'));
+  });
 
   // Funktion zum Wechseln zwischen den Tabs
   function openDAY(day) {
-    // Entfernt die "active" Klasse von allen Tabs und Tab-Inhalten
-    tabcontent.forEach(content => {
-      content.classList.remove("active");
-      content.style.display = "none";
-    });
-
-    tablinks.forEach(link => {
-      link.classList.remove("active");
-    });
-
-    // Fügt "active" zur aktiven Tab und Tab-Inhalt hinzu
-    tabcontent[day].classList.add("active");
-    tabcontent[day].style.display = "block";
-    tablinks[day].classList.add("active");
+    tabcontent.hide().eq(day).show();
+    tablinks.removeClass('active').eq(day).addClass('active');
   }
 
-  // Erste Aktivierung der Tabs
-  openDAY(0);
+  // Erste Aktivierung des aktuellen Tages
+  openDAY((new Date().getDay() || 7) - 1);
 
-  // Funktion zum Abrufen und Verarbeiten des Sendeplans
-  var updateData = function() {
-    var nextUpdate = 3600000 - new Date().getTime() % 3600000;
+  // AJAX-Aufruf zum Abrufen des Sendeplans
+  function updateData() {
+    const nextUpdate = 3600000 - new Date().getTime() % 3600000;
     setTimeout(updateData, nextUpdate);
 
     $.ajax({
       type: "GET",
-      url: "https://api.laut.fm/station/" + station_name + "/schedule",
+      url: `https://api.laut.fm/station/${config.station_name}/schedule`,
       dataType: "json",
       timeout: 10000
-    }).done(function(schedule) {
-      // Hole den aktuellen Wochentag
-      var get_day = (new Date().getDay() || 7) - 1;
-      openDAY(get_day); // Standardmäßig den aktuellen Tag öffnen
+    })
+    .done(renderSchedule)
+    .fail(handleError);
+  }
 
-      if (schedule[0].end_time === null) {
-        document.getElementsByClassName('lfm_schedule')[0].style.display = 'inline-block';
-        document.getElementById('lfm_schedule').innerHTML = '<div class="lfm_schedule_no_entry">Der Sendeplan kann aktuell nicht angezeigt werden</div>';
-      } else {
-        document.getElementsByClassName('lfm_schedule')[0].style.display = 'inline-block';
-        var no_entry = '<div class="no_entry">Leider keine Sendung</div>';
-        var days = ['mon', 'tue', 'wed', 'thu', 'fri', 'sat', 'sun'];
-        var days_buffer = {mon: [], tue: [], wed: [], thu: [], fri: [], sat: [], sun: []};
-        var groupLength = schedule.length;
+  // Fehlerbehandlung
+  function handleError(xhr, status, error) {
+    $('.lfm_schedule').html("Fehler beim Laden der laut.fm - API");
+    console.warn(`Error (API): ${xhr.status} ${error} - ${xhr.responseText}`);
+  }
 
-        schedule.forEach((schedule_entry, index) => {
-          var start_time = schedule_entry.hour.toString().padStart(2, '0') + ':00';
-          var end_time = schedule_entry.end_time ? schedule_entry.end_time.toString().padStart(2, '0') + ':00' : "";
-          var entryindex = index + 1 === groupLength ? "theend" : (index + 1);
+  // Rendering des Sendeplans
+  function renderSchedule(schedule) {
+    const currentDay = (new Date().getDay() || 7) - 1;
+    const currentHour = new Date().getHours();
+    openDAY(currentDay);
 
-          // Wenn Bilder angezeigt werden sollen und Debug aktiviert ist
-          var pl_img = "";
-          if (images) {
-            if (show_img_name) {
-              pl_img = `<td class="lfm_api_schedule_td_img lfm_schedule_td" rowspan="2"><img class="lfm_api_schedule_img" src="${images_url}${schedule_entry.id}.png" onerror="this.onerror=null;this.src='${images_url}${schedule_entry.id}.png';" alt="${schedule_entry.id}.png"></td>`;
-            } else {
-              pl_img = `<td class="lfm_api_schedule_td_img lfm_schedule_td" rowspan="2"><img class="lfm_api_schedule_img" src="${images_url}${schedule_entry.id}.png" onerror="this.onerror=null;this.src='${images_url}${img_default}';" alt="Kein Bild"></td>`;
-            }
-          }
+    if (!schedule.length || schedule[0].end_time === null) {
+      $('#lfm_schedule').html('<div class="lfm_schedule_no_entry">Der Sendeplan kann aktuell nicht angezeigt werden</div>');
+      return;
+    }
 
-          // Bereitstellung des Sendeplans für den entsprechenden Tag
-          if (schedule_entry.day === days[get_day] && schedule_entry.hour <= new Date().getHours()) {
-            if (entryindex === "theend" || schedule[entryindex].day !== days[get_day] || schedule[entryindex].hour > new Date().getHours()) {
-              days_buffer[schedule_entry.day].push(`<table class="lfm_api_schedule_table_playlist_now"><tr><td rowspan="2" class="lfm_api_schedule_time lfm_schedule_td"><span class="onair_symbol">ON AIR</span><br><span class="onair_time">${start_time} Uhr</span></td>${pl_img}<td rowspan="1" class="lfm_api_schedule_playlist_name lfm_schedule_td"><b>${schedule_entry.name}</b></td><td rowspan="1" class="lfm_api_schedule_playlist_description lfm_schedule_td">${schedule_entry.description}</td></tr></table>`);
-            } else {
-              days_buffer[schedule_entry.day].push(`<table class="lfm_api_schedule_table"><tr><td rowspan="2" class="lfm_api_schedule_time lfm_schedule_td">${start_time} Uhr</td>${pl_img}<td rowspan="1" class="lfm_api_schedule_playlist_name lfm_schedule_td"><b>${schedule_entry.name}</b></td><td rowspan="1" class="lfm_api_schedule_playlist_description lfm_schedule_td">${schedule_entry.description}</td></tr></table>`);
-            }
-          } else {
-            days_buffer[schedule_entry.day].push(`<table class="lfm_api_schedule_table"><tr><td rowspan="2" class="lfm_api_schedule_time lfm_schedule_td">${start_time} Uhr</td>${pl_img}<td rowspan="1" class="lfm_api_schedule_playlist_name lfm_schedule_td"><b>${schedule_entry.name}</b></td><td rowspan="1" class="lfm_api_schedule_playlist_description lfm_schedule_td">${schedule_entry.description}</td></tr></table>`);
-          }
-        });
+    const daysBuffer = {mon: [], tue: [], wed: [], thu: [], fri: [], sat: [], sun: []};
+    const noEntry = '<div class="no_entry">Leider keine Sendung</div>';
 
-        // Daten in die entsprechenden Tabs einfügen
-        tabcontent.forEach((tab, index) => {
-          tab.innerHTML = days_buffer[days[index]].length >= 1 ? days_buffer[days[index]].join('') : no_entry;
-        });
-      }
-    }).fail(function(xhr, status, error) {
-      $('.lfm_schedule').html("Fehler beim Laden der laut.fm - API");
-      console.warn('Warning (API): Schedule reports: ' + xhr.status + ' ' + error);
+    schedule.forEach((entry, index) => {
+      const startTime = `${entry.hour.toString().padStart(2, '0')}:00`;
+      const isOnAir = (currentDay === days.indexOf(entry.day) && currentHour >= entry.hour) &&
+                      (index + 1 === schedule.length || schedule[index + 1].hour > currentHour || schedule[index + 1].day !== entry.day);
+      const imgHtml = config.displayImages ? getImageHtml(entry.id) : '';
+      const entryHtml = generateScheduleEntry(entry, startTime, imgHtml, isOnAir);
+      daysBuffer[entry.day].push(entryHtml);
     });
-  };
+
+    Object.keys(daysBuffer).forEach((day, index) => {
+      tabcontent.eq(index).html(daysBuffer[day].length ? daysBuffer[day].join('') : noEntry);
+    });
+  }
+
+  // HTML für den Sendeplan-Eintrag
+  function generateScheduleEntry(entry, startTime, plImg, isOnAir) {
+    return `
+      <table class="${isOnAir ? "lfm_api_schedule_table_playlist_now" : "lfm_api_schedule_table"}">
+        <tr>
+          <td rowspan="2" class="lfm_api_schedule_time lfm_schedule_td">
+            ${isOnAir ? '<span class="onair_symbol">ON AIR</span><br>' : ''}
+            <span class="${isOnAir ? "onair_time" : ""}">${startTime} Uhr</span>
+          </td>
+          ${plImg}
+          <td rowspan="1" class="lfm_api_schedule_playlist_name lfm_schedule_td">
+            <b>${entry.name}</b>
+          </td>
+          ${entry.description ? `<td rowspan="1" class="lfm_api_schedule_playlist_description lfm_schedule_td">${entry.description}</td>` : ''}
+        </tr>
+      </table>
+    `;
+  }
+
+  // Bild HTML für den Sendeplan
+  function getImageHtml(id) {
+    const imgSrc = `${config.imagesUrl}${id}.png`;
+    const imgAlt = `${id}.png`;
+    const fallbackSrc = `${config.imagesUrl}${config.defaultImage}`;
+    return `
+      <td class="lfm_api_schedule_td_img lfm_schedule_td" rowspan="2">
+        <img class="lfm_api_schedule_img" src="${imgSrc}" alt="${imgAlt}" onerror="this.src='${fallbackSrc}'">
+      </td>
+    `;
+  }
 
   // Initiales Laden der Daten
   updateData();
